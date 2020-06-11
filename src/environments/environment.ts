a99803407d14f332dfc9f34acd6e3329e7cc91b5
jhaseel <alanjhaseel@hotmail.com>
2020-06-11 16:16:51 -0500

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAR9XS_X8puHt60rJGNR0DXmOrWFiEjwoU",
-    authDomain: "mike-goodstadt.firebaseapp.com",
-    databaseURL: "https://mike-goodstadt.firebaseio.com",
-    projectId: "mike-goodstadt",
-    storageBucket: "mike-goodstadt.appspot.com",
-    messagingSenderId: "610369153792",
-    appId: "1:610369153792:web:48900a10f5399cbf"
+    apiKey: "x",
+    authDomain: "x",
+    databaseURL: "x",
+    projectId: "x",
+    storageBucket: "x",
+    messagingSenderId: "x",
+    appId: "x"
   }
 };
